483cd9e3838f307fb35f55efb407d295e8f71769
fabricio <fnmamao@gmail.com>
2020-05-15 16:22:33 -0300

    exercicio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            Console.WriteLine("digite uma frase:");
+            frase = Console.ReadLine();
+            Console.WriteLine("o cebolinha diria:");
+            Console.WriteLine(frase.Replace("r" , "l"));
+        }
+    }
+}
